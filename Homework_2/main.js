2cb66f4434c84da43eb8cfadd2b239dbd1d71c11
Tatiana-Dm <83395904+Tatiana-Dm@users.noreply.github.com>
2021-05-18 11:32:42 +0300

    Update main.js

--- Homework_2/main.js
+++ Homework_2/main.js
@@ -1,92 +1,80 @@
-/*
-# 1) Создать массив содержащий все типы данных которые мы изучили
-# - спрашиваем у пользователя 2 операнда (2 промта).
-# - первым будет добавляемый тип, вторым экшен для преобразования соответствующего типа.
-# - добавляем готовый результат в массив.
-# - в конце всех операций выводим получившийся массив в консоль.
-# - массив должен содержать все 8 типов данных JavaScript.
-#
-# 2) Написать скрипт который будет выполнять математические операции
-# - спрашиваем у пользователя 3 операнда (3 промта).
-# - написать функции для всех операций (сложение, вычитание, умножение, деление, деление по модулю).
-#   Каждая функция считает результат и выводит в консоль "Результат [название операции]: 2+3 = 5",
-#   где 2 и 3 то что ввел пользователь в первых двух промтах, а "+" это операция для которой написана функция,
-#   это не обязательно должен быть символ операции (+, -, *, /, %) может быть и текст. 
-# - использовать if else или switch case для вызова необходимой функции. 
-*/
+// Задача №1
+// Дано => массив различный данных (у нас это в основном типы просто для разнообразия).
+// Необходимо => c помощью цикла for вывести в консоль только те элементы массива
+// которые являются ложными (falsy) а так же их порядковый номер в массиве (или его еще называют индексом)
+// пример вывода в консоль для одного из элементов - undefined, index - 1
+// Где:
+// 'undefined' - это элемент массива;
+// ', ' - запятая и пробел просто для разделения значений
+// 'index - ' - это просто строка которая обозначает что следом идет порядковый номер
+// '1' - это сам порядковый номер массива (индекс) о чем предупреждает строка выше
 
-//1
+let data = [
+    null,
+    undefined,
+    false,
+    'string',
+    0,
+    9999999999999999n,
+    Symbol('id'),
+    { itIsObject: true },
+    [0, 11, 22, 33],
+];
 
-let array = [null, undefined];
-
-
-let questionOne = +prompt('Сколько тебе лет?');
-array.push(Number(questionOne));
-
-let questionTwo = prompt('Как тебя зовут?');
-array.push(String(questionTwo));
-
-let questionThree = confirm('Ты еще на сайте?');
-array.push(Boolean(questionThree));
-
-let questionFour = prompt('Используешь id или class?');
-array.push(Symbol(questionFour));
-
-let questionFive = prompt('123n');
-array.push(BigInt(questionFive));
-
-let questionSix = prompt('Как зовут домашнего питомца?');
-array.push(Object({name:(questionSix)}));
-
-console.log(array);
-
-
-//2
-
-function addition (a, b) {
-    let result = a + b;
-    console.log('Результат сложения ' + 'a' +  '+' + 'b' + ' = ' + result);
+for (let i = 0; i < data.length; i++) {
+	if (!data[i]) {
+		console.log(data[i] + ', ' + 'index - ' + data.indexOf(data[i]));
+	}
 }
-// console.log(addition(5, 10));
 
-function subtraction (a, b) {
-    let result = a - b;
-    console.log('Результат вычитания ' + 'a' +  '-' + 'b' + ' = ' + result);
+// Задача №2
+// Дано => все тот же массив 'data'
+// Необходимо => используя все тот же цикл for а так же логические операторы
+// вывести в консоль только те элементы массива которые являются или объектом (object) или массивом
+
+let data = [
+    null,
+    undefined,
+    false,
+    'string',
+    0,
+    9999999999999999n,
+    Symbol('id'),
+    { itIsObject: true },
+    [0, 11, 22, 33],
+];
+
+for(let i = 0; i < data.length; i++) {
+     if (typeof data[i] === 'object' || typeof data[i] === 'array') {
+         console.log(data[i]);
+     }
 }
-// console.log(subtraction(8, 2));
 
-function multiplication (a, b) {
-    let result = a * b;
-    console.log('Результат умножения ' + 'a' +  '*' + 'b' + ' = ' + result);
-}
-// console.log(multiplication(3, 4));
+// Задача №3
+// Существует пустой объект 'personInfo'
+// Необходимо заполнить его информацией и использовать при этом минимум кода
+// Полная свобода действий, никаких ограничений по использованию функций / методов / циклов для заполнения объекта
+// Базовый объект можно редактировать / модифицировать как вам удобно
 
-function division (a, b) {
-    let result = a / b;
-    console.log('Результат деления ' + 'a' +  '/' + 'b' + ' = ' + result);
-}
-// console.log(division(20, 4));
-
-function divisionModulo (a, b) {
-    let result = a % b;
-    console.log('Результат остатка от деления ' + 'a' +  '/' + 'b' + ' = ' + result);
-}
-// console.log(divisionModulo(10, 3));
-
-let a = +prompt('Введите значение a...');
-
-let b = +prompt('Введите значение b...');
-
-let operation = prompt('Вы хотите сделать: сложение(+), вычитание(-), умножение(*), деление(/), деление по модулю(%)?');
-
-if (operation == 'сложение' || operation == '+') {
-    console.log(addition(a, b));
-} else if (operation == 'вычитание' || operation == '-') {
-    console.log(subtraction(a, b));
-} else if (operation == 'умножение' || operation == '*') {
-    console.log(multiplication(a, b));
-} else if (operation == 'деление' || operation == '/') {
-    console.log(division(a, b));
-} else if (operation == 'деление по модулю' || operation == '%') {
-    console.log(divisionModulo(a, b));
+let personInfo = {
+    firstName: null,
+    surName: null,
+    sex: null,
+    age: null,
+    weight: null,
+    height: null,
+    isMarried: null,
+    hasChildren: null,
 };
+
+personInfo.firstName = (prompt('Напиши свою Фамилию'));
+personInfo.surName = (prompt('Как тебя зовут?'));
+personInfo.sex = (prompt('Ты Мужчина ли Женщина?'));
+personInfo.age = (+prompt('Сколько тебе лет?'));
+personInfo.weight = (+prompt('Какой у тебя Обхват талии, (см)?'));
+personInfo.height = (+prompt('Какой у тебя Рост, (см)?'));
+personInfo.isMarried = (prompt('Ты Женат/Замужем?'));
+personInfo.hasChildren = (prompt('У тебя есть дети?'));
+
+console.log(personInfo);
+
